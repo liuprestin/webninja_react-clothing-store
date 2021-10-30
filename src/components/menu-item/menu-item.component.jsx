58e11419a678a3a40673d08eef6f0f4e57bfb890
lexbit <liuprestin@gmail.com>
2021-10-30 15:41:55 -0700

    updating menu-items to naviagate with withRouter

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'; //higher level component
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (  //destructure the title prop here 
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (  //destructure the title prop here 
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{
-        backgroundImage: `url(${imageUrl})`
-    }} />
+        backgroundImage: `url(${imageUrl})` }}/>
                 <div className='content'>
                     <h1 className='title'>{title.toUpperCase()}</h1>
                     <span className='subtitle'>Shop Now</span>
@@ -13,4 +14,4 @@ const MenuItem = ({title, imageUrl, size}) => (  //destructure the title prop he
             </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem); //here we avoid the 'prop-tunneling' pattern - via. withRouter
